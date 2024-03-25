c51e44d83c11abea5190be3640b01a38b959b699
Miriama Svitkova <miriama.svitkova@reaktor.com>
2024-03-25 13:09:52 +0100

    add vite

---
+++ age-calculator-app/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/miri52">Miri</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
